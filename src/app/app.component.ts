aae4b1b95997f72977b82c44c41a51cc0ff6e1e1
Nima Kanani <nkpepello@gmail.com>
2020-10-20 19:52:27 +0330

    Assingment Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent {
+  username: string = '';
+
+  onReset(): void {
+    this.username = '';
+  }
+}
