05fd574d71d8af46f0b58fd2f45c127abcdc1537
Nima <nimappl@hotmail.com>
2020-10-20 13:59:28 +0330

    Assignment Done

--- src/app/SuccessAlert/success.component.ts
+++ src/app/SuccessAlert/success.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-success',
-  templateUrl: './success.component.html',
-  styleUrls: ['./success.component.css']
+    selector: 'app-success',
+    templateUrl: './success.component.html',
+    styleUrls: ['./success.component.css']
 })
+
 export class SuccessComponent { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { WarningComponent } from './WarningAlert/warning.component';
 import { SuccessComponent } from './SuccessAlert/success.component';
+import { WarningComponent } from './WarningAlert/warning.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WarningComponent,
-    SuccessComponent
+    SuccessComponent,
+    WarningComponent
   ],
   imports: [
     BrowserModule
