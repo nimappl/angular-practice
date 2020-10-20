061161e8421779c1fad12a05b359d72cf15fdcee
Nima Kanani <nkpepello@gmail.com>
2020-10-20 23:48:11 +0330

    Assignment Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  displaySecret: boolean = false;
+  displaySecret: boolean = true;
   clicksLog: number[] = [];
 
   addToLog(): void {
