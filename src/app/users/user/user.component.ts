1cd358414b18259912caf5cb749f546378273680
Nima <nimappl@hotmail.com>
2020-11-07 12:21:43 +0330

    Minor Change

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -20,8 +20,8 @@ export class UserComponent implements OnInit, OnDestroy {
     };
     this.paramsSubscription = this.route.params
       .subscribe((params: Params) => {
-        this.user.id = params['id'];
-        this.user.name = params['name'];
+        this.user.id = params.id;
+        this.user.name = params.name;
     });
   }
 
