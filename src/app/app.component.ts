00e1cb1408dbaf19748704687c2905b16fe7a308
Nima <nimappl@hotmail.com>
2020-11-09 15:53:45 +0330

    Template Driven Approach Complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') testForm: NgForm;
+  defaultQuestion = 'pet';
+  answer = '';
+  genders = ['male', 'female'];
+  submitted = false;
+  submittedData = {
+    userData: {
+      username: '',
+      email: ''
+    },
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.testForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: '',
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'female'
+    // });
+
+    this.testForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
+  }
+
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
+  onSubmit() {
+    this.submittedData.userData.username = this.testForm.value.userData.username;
+    this.submittedData.userData.email = this.testForm.value.userData.email;
+    this.submittedData.secretQuestion = this.testForm.value.secret;
+    this.submittedData.answer = this.testForm.value.answer;
+    this.submittedData.gender = this.testForm.value.gender;
+    this.submitted = true;
+    this.testForm.reset();
   }
 }
